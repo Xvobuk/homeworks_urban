bffb1c3112a978cc873afe1045353b2e1130fb66
Vadim Yakubovsky <54107078+Xvobuk@users.noreply.github.com>
2024-10-08 19:10:34 +0300

    Add files via upload

---
+++ module_3_homework/module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(5)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [42, 'Пример', False]
+values_dict = {'a': 100, 'b': 'Новая строка', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
