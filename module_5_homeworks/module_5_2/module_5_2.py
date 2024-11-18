b773be4c64a71682f9cb07c6159553b83409bd81
Vadim Yakubovsky <54107078+Xvobuk@users.noreply.github.com>
2024-11-19 02:59:46 +0300

    Add files via upload

---
+++ module_5_homeworks/module_5_2/module_5_2.py
@@ -0,0 +1,13 @@
+from house import House
+
+# Создание объектов класса House
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# Демонстрация __str__
+print(h1)
+print(h2)
+
+# Демонстрация __len__
+print(len(h1))
+print(len(h2))
