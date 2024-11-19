580a865dbb114edf774ef7c02231aae0de468bd1
Vadim Yakubovsky <54107078+Xvobuk@users.noreply.github.com>
2024-11-19 03:10:43 +0300

    Add files via upload

--- module_3_homework/module_3_5.py
+++ module_3_homework/module_3_5.py
@@ -2,17 +2,23 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
 
+# Валидация:
+    if first == 0:
+        if len(str_number) > 1:
+            return get_multiplied_digits(int(str_number[1:]))
+        else:
+            return 1
+
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
-try:
-    result_3 = get_multiplied_digits('Абоба') # В такие программы можно добавлять исключения, но мы ещё их не прошли)))
-    # Хотя, ладно, тут можно сделать обычную валидацию, так даже лучше будет
-except:
-    result = get_multiplied_digits(40203)
-    result_2 = get_multiplied_digits(88005553535)
+#Проверяем разные значения
+result_1 = get_multiplied_digits(40203)
+result_2 = get_multiplied_digits(88005553535)
+result_3 = get_multiplied_digits(240)
 
-    print(result)
-    print(result_2)
+print(result_1)
+print(result_2)
+print(result_3)
