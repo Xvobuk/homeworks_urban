74cc5bed36f8523148811d1204744e3c337451fb
Vadim Yakubovsky <54107078+Xvobuk@users.noreply.github.com>
2024-12-10 01:27:51 +0300

    Add files via upload

---
+++ module_1_homework/module_1_1/main.py
@@ -0,0 +1,23 @@
+# 1st program
+result_1 = (9 ** 0.5) * 5
+print(result_1)
+
+# 2nd program
+result_2 = (9.99 > 9.98) and (1000 != 1000.1)
+print(result_2)
+
+# 3rd program
+expr1 = 2 * 2 + 2
+expr2 = 2 * (2 + 2)
+comparison_result = expr1 == expr2
+print(expr1)
+print(expr2)
+print(comparison_result)
+
+# 4th program
+number_str = '123.456'
+number_float = float(number_str)
+shifted_number = number_float * 10
+whole_part = int(shifted_number)
+first_decimal = whole_part % 10
+print(first_decimal)
