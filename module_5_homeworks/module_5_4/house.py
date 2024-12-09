f5931613358b331a51e528c524d385d495fde2b5
Vadim Yakubovsky <54107078+Xvobuk@users.noreply.github.com>
2024-12-10 00:45:31 +0300

    Add files via upload

---
+++ module_5_homeworks/module_5_4/house.py
@@ -0,0 +1,14 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        cls.houses_history.append(args[0])
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
